3f6f0d322f0d4dc0a550cf27f1a0096aebff936f
Andy Yu <ayu303@gatech.edu>
2024-02-11 02:48:17 -0500

    Workaround max token length
    Break article text into smaller chunks to work around the 512-token limit of T5.

--- inference.py
+++ inference.py
@@ -1,17 +1,34 @@
 from transformers import T5Tokenizer, T5ForConditionalGeneration
 import articleFetch
 
+def breakText(text, n): 
+    """
+    Break string into list of n-word long strings
+    """
+    textList = text.split(" ")
+    # looping till length l 
+    for i in range(0, len(textList), n):  
+        yield " ".join(textList[i:i + n]) 
+  
+
 
 def getArticleSummary(tags: list[str]):
     tokenizer = T5Tokenizer.from_pretrained("t5-base")
     model = T5ForConditionalGeneration.from_pretrained("t5-base")
 
     article = articleFetch.getPaperText(tags)
-    prompt = "summarize: " + article
-    #print(prompt)
-    input_ids = tokenizer(prompt,return_tensors="pt").input_ids
+    maxWordLength = 300
+    if len(article.split(" ")) > maxWordLength:
+        articleList = list(breakText(article, maxWordLength))
+    else: 
+        articleList = [article]
+    summarySentences = []
+    for chunk in articleList:
+        prompt = "summarize: " + chunk
+        #print(prompt)
+        input_ids = tokenizer(prompt,return_tensors="pt").input_ids
+        outputs = model.generate(input_ids, max_length = 512)
+        summarySentences.append(tokenizer.decode(outputs[0], skip_special_tokens=True))
+    return " ".join(summarySentences)
 
-    outputs = model.generate(input_ids, max_length = 500)
-    return tokenizer.decode(outputs[0], skip_special_tokens=True)
-
-#print(getArticleSummary(["e-health", "diabetes"]))
+print(getArticleSummary(["e-health", "diabetes"]))
