768cfc487d8f1ae6fc4a9b6278676975714a9964
Andy Yu <ayu303@gatech.edu>
2024-02-11 02:29:51 -0500

    increase model size

--- inference.py
+++ inference.py
@@ -3,12 +3,15 @@ import articleFetch
 
 
 def getArticleSummary(tags: list[str]):
-    tokenizer = T5Tokenizer.from_pretrained("t5-small")
-    model = T5ForConditionalGeneration.from_pretrained("t5-small")
+    tokenizer = T5Tokenizer.from_pretrained("t5-base")
+    model = T5ForConditionalGeneration.from_pretrained("t5-base")
 
-    prompt = "sumarize: " + articleFetch.getPaperText(tags)
-    print(prompt)
+    article = articleFetch.getPaperText(tags)
+    prompt = "summarize: " + article
+    #print(prompt)
     input_ids = tokenizer(prompt,return_tensors="pt").input_ids
 
     outputs = model.generate(input_ids, max_length = 500)
     return tokenizer.decode(outputs[0], skip_special_tokens=True)
+
+#print(getArticleSummary(["e-health", "diabetes"]))
